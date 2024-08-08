3d7b7b85b51c0d34a1e8dec100124a5c578ede22
Praneesha <pranesha182004@gmail.com>
2024-08-08 13:16:25 +0530

    changed blog layout

--- src/Components/BlogsRelated/BlogsSection1.jsx
+++ src/Components/BlogsRelated/BlogsSection1.jsx
@@ -1,16 +1,16 @@
 import React from "react";
 import {
   Box,
-  Card,
-  CardBody,
-  Grid,
-  GridItem,
-  SimpleGrid,
-  Text,
-  Image,
-  Heading,
-  CardFooter,
-  VStack,
+    Button,
+    Card,
+    CardBody,
+    CardFooter,
+    Container,
+    HStack,
+    Image,
+    SimpleGrid,
+    Text,
+    VStack,
 } from "@chakra-ui/react";
 import { FiArrowUpRight } from "react-icons/fi";
 import theme from "../../theme";
@@ -35,67 +35,77 @@ const BlogPost = [
 const BlogsSection1 = () => {
   return (
     <>
-      <Box
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        fontFamily={theme.fonts.body}
-      >
-        <Box w="80%" mt="100px">
-          <Heading fontSize="2.5rem" color={theme.colors.col.text} mt='20px' p={3} >
-            Latest Blogs
-          </Heading>
-          <SimpleGrid
-            w={"100%"}
-            columns={["1", "1", "2", "2", "3"]}
-            mt={["2.5rem"]}
-            spacing={[10, 10, 5, 10, 4]}
-          >
-            {BlogPost.map((blog, index) => (
-              <Card
-                key={index}
-                bgColor={"rgb(246,247,249)"}
-                overflow={"hidden"}
-                p={3}
-                borderRadius={"10px"}
-              >
-                {/* blogimage */}
-                <Box w="100%" h="300px">
-                    <Image src={blog.imageUrl} alt="blog1" objectFit="cover" h='100%' w='100%' borderTopRadius='10px'/>
+      <Box width="100%" my={["5rem", "5rem", "7rem", "5rem", "10rem"]}>
+       <Container
+         w={["100%", "100%", "95%", "95%"]}
+         maxW="1500px"
+         marginX="auto"
+       >
+         <HStack
+           w={['100%','90%','100%']}
+           mx={'auto'}
+           justifyContent={["space-between"]}
+           gap={["1rem"]}
+           alignItems={"flex-end"}
+         >
+           <Box
+             fontSize={["1.2rem", "1.5rem", "1.8rem", "2.5rem"]}
+             fontWeight={"600"}
+             color={theme.colors.col.secondary}
+             pl={3}
+           >
+             Latest Blogs
+           </Box>
+           <Box>
+             <Button
+               size={["sm", "sm", "md",'lg']}
+               fontSize={["0.8rem", "0.8rem", "0.9rem"]}
+               variant={"outline"}
+               colorScheme="orange" 
+             >
+               View all
+             </Button>
+           </Box>
+         </HStack>
+ 
+         <SimpleGrid  w={'100%'}  columns={['1','1','2','2','4']} mt={['3rem']} spacing={[10,10,5,10,4]}>
+           {
+             BlogPost?.slice(0,4).map((item,index)=>(
+               <Card key={index} bgColor={'rgb(245,247,248)'} overflow={'hidden'} p={3} borderRadius={'10px'}>
+               <Box w={'100%'} h={['250px']} position={['relative']} overflow={'hidden'} borderRadius={'10px'}>
+                  <Image w={'100%'} h={'100%'} objectFit={'cover'} src={item.imageUrl} />
+               </Box>
+ 
+              <CardBody px={2}>
+                   <VStack alignItems={'flex-start'}>
+                     <Box fontSize={'1.5rem'} fontWeight={'600'} color={theme.colors.col.secondary}>
+                    {item.BlogHeading.length >= 18 ? `${item.BlogHeading.substring(0,19)}...`: item.BlogHeading}
+                     </Box>
+                     <Text lineHeight={['1.7rem']} fontSize={['0.9rem']} textAlign='justify'>
+                      {item.desc.length >= 150 ? `${item.desc.substring(0,150)}...` : item.desc}
+                     </Text>
+                   </VStack>
+              </CardBody>
+ 
+              <CardFooter py={'1'} px={2}>
+                   <HStack  w={'100%'} justifyContent={'space-between'}>
+                   <Box  bgColor={theme.colors.col.secondary} color={'white'} boxShadow={'0 1px 3px rgba(0,0,0,0.10), 0 1px 2px rgba(0,0,0,0.10)'} borderRadius={'10px'} p={2} fontWeight={'600'} pointerEvents={'none'} fontSize={'0.9rem'}>
+                     {item.date}
                   </Box>
-                <CardBody px='2'>
-                  <VStack  alignItems='flex-start'>
-                    {/* blogheading */}
-                    <Heading color={theme.colors.col.secondary}>
-                      {
-                       blog.BlogHeading.length >= 18 ? `${blog.BlogHeading.substring(0,18)}...`: blog.BlogHeading
-                       }
-                    </Heading>
-                     {/* blogdesc */}
-                    <Text fontSize='0.9rem' textAlign='justify'>
-                      {
-                        blog.desc.length>=150?`${blog.desc.substring(0,150)}...`:blog.desc
-                      }
-                      </Text>
-                  </VStack>
-                </CardBody>
-                <CardFooter py='1' px='2'>
-                  <Box display="flex" justifyContent='space-between' alignItems='center' w='100%'>
-                    {/* blogdate */}
-                    <Box bgColor={theme.colors.col.text} color={'white'}  borderRadius={'10px'} p={'6'} fontWeight={'600'} pointerEvents={'none'} fontSize={'1.2rem'} textAlign='center' h='40px' display='flex' alignItems='center' justifyContent='center'>
-                     {blog.date}
-                   </Box>
-                   {/* blogarrow */}
-                    <Box role='group' w='40px' h='40px' bg={theme.colors.col.text} p={6} display='flex' alignItems='center' justifyContent='center' fontSize='1.5rem' color='white' borderRadius='10px'>
-                      <Box _groupHover={{transform:'rotate(45deg)'}} transition={'all 0.2s linear'}><FiArrowUpRight  /></Box>
-                      </Box>
-                  </Box>
-                </CardFooter>
-              </Card>
-            ))}
-          </SimpleGrid>
-        </Box>
-      </Box>
+                       <Box role="group" bgColor={theme.colors.col.secondary} display={'inline-flex'} w={'40px'} h={'40px'}  justifyContent={'center'} alignItems={'center'} fontSize={'1.1rem'} color={'white'} borderRadius={'10px'} boxShadow={'0 1px 3px rgba(0,0,0,0.10), 0 1px 2px rgba(0,0,0,0.10)'} cursor={'pointer'}>
+                        <Box _groupHover={{transform:'rotate(45deg)'}} transition={'all 0.2s linear'}>
+                        <Box><FiArrowUpRight  /></Box>
+                        </Box>
+                       </Box>
+                   </HStack>
+              </CardFooter>
+ 
+          </Card>
+             ))
+           }
+         </SimpleGrid>
+       </Container>
+     </Box>
     </>
   );
 };
